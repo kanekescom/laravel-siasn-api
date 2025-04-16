cc10570b45b5d274e29032d60d4311080ba10b2a
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2025-04-16 14:17:38 +0700

    feat(tests): add phpunit.xml configuration and replace placeholders with GitHub Secrets

--- src/Credentials/Apim.php
+++ src/Credentials/Apim.php
@@ -21,7 +21,6 @@ class Apim implements TokenProvider
 
     public function getToken(): object
     {
-        dd($this->credentialProvider->getCredentials()->username);
         try {
             $credentials = $this->credentialProvider->getCredentials();
             $this->credentialProvider->validateCredentials($credentials);
